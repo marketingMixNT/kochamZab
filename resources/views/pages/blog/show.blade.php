c8018f076f742bfc557619e435889be90fb97cb4
marekgacek45 <marekgacek45@gmail.com>
2024-08-01 09:47:04 +0200

    update blog show page

---
+++ resources/views/pages/blog/show.blade.php
@@ -0,0 +1,55 @@
+<x-layouts.app title="{{ $post->getMetaTitle() }}" description="{{ $post->getMetaDesc() }}">
+
+
+
+    <x-slot:header>
+
+        <header class="max-w-screen-lg mx-auto pt-12 lg:pt-20 px-6 sm:px-12 2xl:px-0 space-y-6">
+            <img src="{{ $post->getThumbnailUrl() }}" alt="{{ $post->title }}" class="aspect-video w-full object-cover">
+
+            <div class="text-center flex flex-col gap-3">
+
+                <h1 class="text-3xl sm:text-4xl lg:text-5xl font-extrabold text-fontBlue dark:text-fontLight ">
+                    {{ $post->title }}</h1>
+
+                <span class="text-sm">{{ $post->getPublishedDate() }}</span>
+            </div>
+        </header>
+    </x-slot:header>
+
+    <x-slot:main>
+
+        <x-base.section narrow class="max-w-screen-lg mx-auto">
+            <article class="prose">
+
+                {!! $post->content !!}
+
+            </article>
+        </x-base.section>
+
+
+        <x-base.section narrow class="max-w-screen-xl mx-auto pb-20">
+
+            <x-ui.heading>Pozostałe artykuły</x-ui.heading>
+
+            <x-base.loop-grid>
+
+                @foreach ($filteredPosts as $post)
+                    <x-post-card :post="$post" />
+                @endforeach
+            </x-base.loop-grid>
+        </x-base.section>
+    </x-slot:main>
+
+
+
+
+
+
+
+
+
+
+
+
+</x-layouts.app>
